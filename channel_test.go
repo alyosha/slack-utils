614682385c580a10a92a50729599747930184b68
Steve Johnson <stephenjohnsonm@gmail.com>
2019-04-27 23:05:58 +0900

    Add other channel tests

--- channel_test.go
+++ channel_test.go
@@ -6,7 +6,6 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	utils "github.com/alyosha/slack-utils"
 	"github.com/nlopes/slack"
 )
 
@@ -14,7 +13,7 @@ func TestCreateChannel(t *testing.T) {
 	testCases := []struct {
 		description       string
 		inviteMembers     []string
-		initMsg           utils.Msg
+		initMsg           Msg
 		respChannelCreate []byte
 		respInviteMembers []byte
 		respPostMsg       []byte
@@ -24,21 +23,21 @@ func TestCreateChannel(t *testing.T) {
 		{
 			description:       "successful channel creation, no additional invites",
 			inviteMembers:     []string{},
-			initMsg:           utils.Msg{},
+			initMsg:           Msg{},
 			respChannelCreate: []byte(channelCreateResp),
 			wantID:            "C0DEL09A5",
 		},
 		{
 			description:       "err creating channel",
 			inviteMembers:     []string{},
-			initMsg:           utils.Msg{},
+			initMsg:           Msg{},
 			respChannelCreate: []byte(channelCreateErrResp),
 			wantErr:           "invalid_name_specials",
 		},
 		{
 			description:       "successful channel creation including additional invites",
 			inviteMembers:     []string{"UABC123EFG"},
-			initMsg:           utils.Msg{},
+			initMsg:           Msg{},
 			respChannelCreate: []byte(channelCreateResp),
 			respInviteMembers: []byte(inviteMembersResp),
 			wantID:            "C0DEL09A5",
@@ -46,7 +45,7 @@ func TestCreateChannel(t *testing.T) {
 		{
 			description:       "successful channel creation, inviting members including self but no error returned",
 			inviteMembers:     []string{"U0G9QF9C6"},
-			initMsg:           utils.Msg{},
+			initMsg:           Msg{},
 			respChannelCreate: []byte(channelCreateResp),
 			respInviteMembers: []byte(cantInviteSelfErrResp),
 			wantID:            "C0DEL09A5",
@@ -54,7 +53,7 @@ func TestCreateChannel(t *testing.T) {
 		{
 			description:       "err inviting members",
 			inviteMembers:     []string{"UABC123EFG"},
-			initMsg:           utils.Msg{},
+			initMsg:           Msg{},
 			respChannelCreate: []byte(channelCreateResp),
 			respInviteMembers: []byte(inviteMembersErrResp),
 			wantErr:           "cant_invite",
@@ -62,7 +61,7 @@ func TestCreateChannel(t *testing.T) {
 		{
 			description:       "successful channel creation including additional invites, successful message post",
 			inviteMembers:     []string{"UABC123EFG"},
-			initMsg:           utils.Msg{Body: "Hey!"},
+			initMsg:           Msg{Body: "Hey!"},
 			respChannelCreate: []byte(channelCreateResp),
 			respInviteMembers: []byte(inviteMembersResp),
 			respPostMsg:       []byte(postMsgResp),
@@ -71,7 +70,7 @@ func TestCreateChannel(t *testing.T) {
 		{
 			description:       "successful channel creation including additional invites, successful message post",
 			inviteMembers:     []string{"UABC123EFG"},
-			initMsg:           utils.Msg{Body: "Hey!"},
+			initMsg:           Msg{Body: "Hey!"},
 			respChannelCreate: []byte(channelCreateResp),
 			respInviteMembers: []byte(inviteMembersResp),
 			respPostMsg:       []byte(postMsgErrResp),
@@ -96,12 +95,16 @@ func TestCreateChannel(t *testing.T) {
 			defer testServ.Close()
 
 			client := slack.New("x012345", slack.OptionAPIURL(fmt.Sprintf("%v/", testServ.URL)))
-			channel := utils.Channel{
+			channel := Channel{
 				UserClient: client,
 				BotClient:  nil,
 			}
 
 			id, err := channel.CreateChannel("general", tc.inviteMembers, tc.initMsg, false)
+
+			if tc.wantErr == "" && err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
 
 			if tc.wantErr != "" {
 				if err == nil {
@@ -116,6 +119,313 @@ func TestCreateChannel(t *testing.T) {
 
 			if id != tc.wantID {
 				t.Fatalf("expected channel id: %s, got: %s", tc.wantID, id)
+			}
+		})
+	}
+}
+
+func TestInviteUsers(t *testing.T) {
+	testCases := []struct {
+		description       string
+		inviteMembers     []string
+		respInviteMembers []byte
+		wantErr           string
+	}{
+		{
+			description:       "successful invite of users",
+			inviteMembers:     []string{"UABC123EFG"},
+			respInviteMembers: []byte(inviteMembersResp),
+		},
+		{
+			description:       "successful invite, no error returned for invite members resp",
+			inviteMembers:     []string{"UABC123EFG", "U0G9QF9C6"},
+			respInviteMembers: []byte(inviteMembersResp),
+		},
+		{
+			description:       "expect error",
+			inviteMembers:     []string{"UABC123EFG"},
+			respInviteMembers: []byte(inviteMembersErrResp),
+			wantErr:           "cant_invite",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			mux := http.NewServeMux()
+			mux.HandleFunc("/channels.invite", func(w http.ResponseWriter, r *http.Request) {
+				_, _ = w.Write(tc.respInviteMembers)
+			})
+
+			testServ := httptest.NewServer(mux)
+			defer testServ.Close()
+
+			client := slack.New("x012345", slack.OptionAPIURL(fmt.Sprintf("%v/", testServ.URL)))
+			channel := Channel{
+				UserClient: client,
+				BotClient:  nil,
+			}
+
+			err := channel.InviteUsers(tc.inviteMembers, "C1H9RESGL")
+
+			if tc.wantErr == "" && err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+
+			if tc.wantErr != "" {
+				if err == nil {
+					t.Fatal("expected error but did not receive one")
+					return
+				}
+				if err.Error() != tc.wantErr {
+					t.Fatalf("expected to receive error: %s, got: %s", tc.wantErr, err)
+					return
+				}
+			}
+		})
+	}
+}
+
+func TestGetChannelMembers(t *testing.T) {
+	testCases := []struct {
+		description     string
+		respChannelInfo []byte
+		wantErr         string
+		wantIDs         []string
+	}{
+		{
+			description:     "successful retrieval of member IDs",
+			respChannelInfo: []byte(channelInfoResp),
+			wantIDs:         []string{"U0G9QF9C6", "U1QNSQB9U"},
+		},
+		{
+			description:     "failure to retrieve member IDs",
+			respChannelInfo: []byte(channelInfoErrResp),
+			wantErr:         "channel_not_found",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			mux := http.NewServeMux()
+			mux.HandleFunc("/channels.info", func(w http.ResponseWriter, r *http.Request) {
+				_, _ = w.Write(tc.respChannelInfo)
+			})
+
+			testServ := httptest.NewServer(mux)
+			defer testServ.Close()
+
+			client := slack.New("x012345", slack.OptionAPIURL(fmt.Sprintf("%v/", testServ.URL)))
+			slackClient := Slack{
+				Client: client,
+			}
+
+			members, err := slackClient.GetChannelMembers("C1H9RESGL")
+
+			if tc.wantErr == "" && err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+
+			if tc.wantErr != "" {
+				if err == nil {
+					t.Fatal("expected error but did not receive one")
+					return
+				}
+				if err.Error() != tc.wantErr {
+					t.Fatalf("expected to receive error: %s, got: %s", tc.wantErr, err)
+					return
+				}
+			}
+
+			if len(members) != len(tc.wantIDs) {
+				t.Fatalf("expected to receive %v ids, got %v instead", len(tc.wantIDs), len(members))
+			}
+
+			for i, member := range members {
+				if tc.wantIDs[i] != member {
+					t.Fatalf("expected to receive id: %v, got: %v", tc.wantIDs[i], member)
+				}
+			}
+		})
+	}
+}
+
+func TestGetChannelMemberEmails(t *testing.T) {
+	testCases := []struct {
+		description     string
+		respChannelInfo []byte
+		respUsersList   []byte
+		wantErr         string
+		wantEmails      []string
+	}{
+		{
+			description:     "successful retrieval of member emails",
+			respChannelInfo: []byte(channelInfoResp),
+			respUsersList:   []byte(usersListResp),
+			wantEmails:      []string{"spengler@ghostbusters.example.com"},
+		},
+		{
+			description:     "failure to retrieve channel info",
+			respChannelInfo: []byte(channelInfoErrResp),
+			wantErr:         "channel_not_found",
+		},
+		{
+			description:     "failure to retrieve users list",
+			respChannelInfo: []byte(usersListErrResp),
+			wantErr:         "invalid_cursor",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			mux := http.NewServeMux()
+			mux.HandleFunc("/channels.info", func(w http.ResponseWriter, r *http.Request) {
+				_, _ = w.Write(tc.respChannelInfo)
+			})
+			mux.HandleFunc("/users.list", func(w http.ResponseWriter, r *http.Request) {
+				_, _ = w.Write(tc.respUsersList)
+			})
+
+			testServ := httptest.NewServer(mux)
+			defer testServ.Close()
+
+			client := slack.New("x012345", slack.OptionAPIURL(fmt.Sprintf("%v/", testServ.URL)))
+			slackClient := Slack{
+				Client: client,
+			}
+
+			emails, err := slackClient.GetChannelMemberEmails("C1H9RESGL")
+
+			if tc.wantErr == "" && err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+
+			if tc.wantErr != "" {
+				if err == nil {
+					t.Fatal("expected error but did not receive one")
+					return
+				}
+				if err.Error() != tc.wantErr {
+					t.Fatalf("expected to receive error: %s, got: %s", tc.wantErr, err)
+					return
+				}
+			}
+
+			if len(emails) != len(tc.wantEmails) {
+				t.Fatalf("expected to receive %v emails, got %v instead", len(tc.wantEmails), len(emails))
+			}
+
+			for i, email := range emails {
+				if tc.wantEmails[i] != email {
+					t.Fatalf("expected to receive email: %v, got: %v", tc.wantEmails[i], email)
+				}
+			}
+		})
+	}
+}
+
+func TestLeaveChannels(t *testing.T) {
+	testCases := []struct {
+		description       string
+		respLeaveChannels []byte
+		wantErr           string
+	}{
+		{
+			description:       "successfully left channels",
+			respLeaveChannels: []byte(channelsLeaveResp),
+		},
+		{
+			description:       "failure to leave channels",
+			respLeaveChannels: []byte(channelsLeaveErrResp),
+			wantErr:           "invalid_auth",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			mux := http.NewServeMux()
+			mux.HandleFunc("/channels.leave", func(w http.ResponseWriter, r *http.Request) {
+				_, _ = w.Write(tc.respLeaveChannels)
+			})
+
+			testServ := httptest.NewServer(mux)
+			defer testServ.Close()
+
+			client := slack.New("x012345", slack.OptionAPIURL(fmt.Sprintf("%v/", testServ.URL)))
+			slackClient := Slack{
+				Client: client,
+			}
+
+			err := slackClient.LeaveChannels([]string{"C1H9RESGL"})
+
+			if tc.wantErr == "" && err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+
+			if tc.wantErr != "" {
+				if err == nil {
+					t.Fatal("expected error but did not receive one")
+					return
+				}
+				if err.Error() != tc.wantErr {
+					t.Fatalf("expected to receive error: %s, got: %s", tc.wantErr, err)
+					return
+				}
+			}
+		})
+	}
+}
+
+func TestArchiveChannels(t *testing.T) {
+	testCases := []struct {
+		description         string
+		respArchiveChannels []byte
+		wantErr             string
+	}{
+		{
+			description:         "successfully archived channels",
+			respArchiveChannels: []byte(channelsArchiveResp),
+		},
+		{
+			description:         "no error returned for already archived channel",
+			respArchiveChannels: []byte(channelAlreadyArchivedErrResp),
+		},
+		{
+			description:         "failure to archive channels",
+			respArchiveChannels: []byte(channelsArchiveErrResp),
+			wantErr:             "invalid_auth",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			mux := http.NewServeMux()
+			mux.HandleFunc("/channels.archive", func(w http.ResponseWriter, r *http.Request) {
+				_, _ = w.Write(tc.respArchiveChannels)
+			})
+
+			testServ := httptest.NewServer(mux)
+			defer testServ.Close()
+
+			client := slack.New("x012345", slack.OptionAPIURL(fmt.Sprintf("%v/", testServ.URL)))
+			slackClient := Slack{
+				Client: client,
+			}
+
+			err := slackClient.ArchiveChannels([]string{"C1H9RESGL"})
+
+			if tc.wantErr == "" && err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+
+			if tc.wantErr != "" {
+				if err == nil {
+					t.Fatal("expected error but did not receive one")
+					return
+				}
+				if err.Error() != tc.wantErr {
+					t.Fatalf("expected to receive error: %s, got: %s", tc.wantErr, err)
+					return
+				}
 			}
 		})
 	}
--- mock_resp.go
+++ mock_resp.go
@@ -102,6 +102,155 @@ const postMsgResp = `{
     }
 }`
 
+const channelInfoResp = `{
+    "ok": true,
+    "channel": {
+        "id": "C1H9RESGL",
+        "name": "busting",
+        "is_channel": true,
+        "created": 1466025154,
+        "creator": "U0G9QF9C6",
+        "is_archived": false,
+        "is_general": false,
+        "name_normalized": "busting",
+        "is_shared": false,
+        "is_org_shared": false,
+        "is_member": true,
+        "is_private": false,
+        "is_mpim": false,
+        "last_read": "1503435939.000101",
+        "latest": {
+            "text": "Containment unit is 98% full",
+            "username": "ecto1138",
+            "bot_id": "B19LU7CSY",
+            "attachments": [
+                {
+                    "text": "Don't get too attached",
+                    "id": 1,
+                    "fallback": "This is an attachment fallback"
+                }
+            ],
+            "type": "message",
+            "subtype": "bot_message",
+            "ts": "1503435956.000247"
+        },
+        "unread_count": 1,
+        "unread_count_display": 1,
+        "members": [
+            "U0G9QF9C6",
+            "U1QNSQB9U"
+        ],
+        "topic": {
+            "value": "Spiritual containment strategies",
+            "creator": "U0G9QF9C6",
+            "last_set": 1503435128
+        },
+        "purpose": {
+            "value": "Discuss busting ghosts",
+            "creator": "U0G9QF9C6",
+            "last_set": 1503435128
+        },
+        "previous_names": [
+            "dusting"
+        ]
+    }
+}`
+
+const usersListResp = `{
+    "ok": true,
+    "members": [
+        {
+            "id": "U0G9QF9C6",
+            "team_id": "T012AB3C4",
+            "name": "spengler",
+            "deleted": false,
+            "color": "9f69e7",
+            "real_name": "spengler",
+            "tz": "America/Los_Angeles",
+            "tz_label": "Pacific Daylight Time",
+            "tz_offset": -25200,
+            "profile": {
+                "avatar_hash": "ge3b51ca72de",
+                "status_text": "Print is dead",
+                "status_emoji": ":books:",
+                "real_name": "Egon Spengler",
+                "display_name": "spengler",
+                "real_name_normalized": "Egon Spengler",
+                "display_name_normalized": "spengler",
+                "email": "spengler@ghostbusters.example.com",
+                "image_24": "https://.../avatar/e3b51ca72dee4ef87916ae2b9240df50.jpg",
+                "image_32": "https://.../avatar/e3b51ca72dee4ef87916ae2b9240df50.jpg",
+                "image_48": "https://.../avatar/e3b51ca72dee4ef87916ae2b9240df50.jpg",
+                "image_72": "https://.../avatar/e3b51ca72dee4ef87916ae2b9240df50.jpg",
+                "image_192": "https://.../avatar/e3b51ca72dee4ef87916ae2b9240df50.jpg",
+                "image_512": "https://.../avatar/e3b51ca72dee4ef87916ae2b9240df50.jpg",
+                "team": "T012AB3C4"
+            },
+            "is_admin": true,
+            "is_owner": false,
+            "is_primary_owner": false,
+            "is_restricted": false,
+            "is_ultra_restricted": false,
+            "is_bot": false,
+            "updated": 1502138686,
+            "is_app_user": false,
+            "has_2fa": false
+        },
+        {
+            "id": "W07QCRPA4",
+            "team_id": "T0G9PQBBK",
+            "name": "glinda",
+            "deleted": false,
+            "color": "9f69e7",
+            "real_name": "Glinda Southgood",
+            "tz": "America/Los_Angeles",
+            "tz_label": "Pacific Daylight Time",
+            "tz_offset": -25200,
+            "profile": {
+                "avatar_hash": "8fbdd10b41c6",
+                "image_24": "https://a.slack-edge.com...png",
+                "image_32": "https://a.slack-edge.com...png",
+                "image_48": "https://a.slack-edge.com...png",
+                "image_72": "https://a.slack-edge.com...png",
+                "image_192": "https://a.slack-edge.com...png",
+                "image_512": "https://a.slack-edge.com...png",
+                "image_1024": "https://a.slack-edge.com...png",
+                "image_original": "https://a.slack-edge.com...png",
+                "first_name": "Glinda",
+                "last_name": "Southgood",
+                "title": "Glinda the Good",
+                "phone": "",
+                "skype": "",
+                "real_name": "Glinda Southgood",
+                "real_name_normalized": "Glinda Southgood",
+                "display_name": "Glinda the Fairly Good",
+                "display_name_normalized": "Glinda the Fairly Good",
+                "email": "glenda@south.oz.coven"
+            },
+            "is_admin": true,
+            "is_owner": false,
+            "is_primary_owner": false,
+            "is_restricted": false,
+            "is_ultra_restricted": false,
+            "is_bot": false,
+            "updated": 1480527098,
+            "has_2fa": false
+        }
+    ],
+    "cache_ts": 1498777272,
+		"response_metadata": {
+			"next_cursor": ""
+    }
+}`
+
+const channelsLeaveResp = `{
+    "ok": true
+}`
+
+const channelsArchiveResp = `{
+    "ok": true
+}`
+
 const channelCreateErrResp = `{
     "ok": false,
     "error": "invalid_name_specials",
@@ -121,4 +270,29 @@ const cantInviteSelfErrResp = `{
 const postMsgErrResp = `{
     "ok": false,
     "error": "too_many_attachments"
+}`
+
+const channelInfoErrResp = `{
+    "ok": false,
+    "error": "channel_not_found"
+}`
+
+const usersListErrResp = `{
+    "ok": false,
+    "error": "invalid_cursor"
+}`
+
+const channelsLeaveErrResp = `{
+    "ok": false,
+    "error": "invalid_auth"
+}`
+
+const channelsArchiveErrResp = `{
+    "ok": false,
+    "error": "invalid_auth"
+}`
+
+const channelAlreadyArchivedErrResp = `{
+    "ok": false,
+    "error": "already_archived"
 }`
