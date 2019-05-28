74098914a02cc3bf49eb650bace720ecf76ff267
Steve Johnson <stephenjohnsonm@gmail.com>
2019-05-29 03:19:05 +0900

    Add first block helper method and test

---
+++ block_test.go
@@ -0,0 +1,71 @@
+package utils
+
+import (
+	"testing"
+
+	"github.com/kylelemons/godebug/pretty"
+	"github.com/nlopes/slack"
+)
+
+const (
+	mockActionID             = "fAkE123"
+	mockValue                = "click_me_btn"
+	fakeStyle    slack.Style = "fake"
+)
+
+var mockTextObj = &slack.TextBlockObject{
+	slack.PlainTextType,
+	"Click me",
+	false,
+	false,
+}
+
+func TestNewButtonWithStyle(t *testing.T) {
+	testCases := []struct {
+		description string
+		style       slack.Style
+		wantButton  *slack.ButtonBlockElement
+	}{
+		{
+			description: "returns default style button, no error",
+			style:       slack.StyleDefault,
+			wantButton: &slack.ButtonBlockElement{
+				Type:     slack.METButton,
+				ActionID: mockActionID,
+				Text:     mockTextObj,
+				Value:    mockValue,
+				Style:    slack.StyleDefault,
+			},
+		},
+		{
+			description: "returns danger style button, no error",
+			style:       slack.StyleDanger,
+			wantButton: &slack.ButtonBlockElement{
+				Type:     slack.METButton,
+				ActionID: mockActionID,
+				Text:     mockTextObj,
+				Value:    mockValue,
+				Style:    slack.StyleDanger,
+			},
+		},
+		{
+			description: "returns primary style button, no error",
+			style:       slack.StylePrimary,
+			wantButton: &slack.ButtonBlockElement{
+				Type:     slack.METButton,
+				ActionID: mockActionID,
+				Text:     mockTextObj,
+				Value:    mockValue,
+				Style:    slack.StylePrimary,
+			},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			btn := NewButtonWithStyle(mockActionID, mockValue, mockTextObj, tc.style)
+			if diff := pretty.Compare(btn, tc.wantButton); diff != "" {
+				t.Fatalf("expected to receive button: %v, got: %v", tc.wantButton, btn)
+			}
+		})
+	}
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/alyosha/slack-utils
 require (
 	github.com/davecgh/go-spew v1.1.1
 	github.com/gorilla/websocket v1.4.0 // indirect
+	github.com/kylelemons/godebug v1.1.0
 	github.com/nlopes/slack v0.5.1-0.20190515005541-e2954b1409b0
 	github.com/pkg/errors v0.8.1 // indirect
 	golang.org/x/sync v0.0.0-20190423024810-112230192c58
