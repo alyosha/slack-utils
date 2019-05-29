92d3dacd00a48b3acdf802266c7b0dc8c4b12627
Steve Johnson <stephenjohnsonm@gmail.com>
2019-05-30 03:33:40 +0900

    Update datepicker test

--- block.go
+++ block.go
@@ -12,7 +12,7 @@ func NewButtonWithStyle(actionID, value string, textObj *slack.TextBlockObject, 
 	return btn
 }
 
-func NewDatePickerWithOpts(actionID, placeholder string, initialDate time.Time) *slack.DatePickerBlockElement {
+func NewDatePickerWithOpts(actionID string, placeholder *slack.TextBlockObject, initialDate time.Time) *slack.DatePickerBlockElement {
 	picker := slack.NewDatePickerBlockElement(actionID)
 	dateStr := initialDate.Format("2006-01-02")
 	picker.Placeholder = placeholder
